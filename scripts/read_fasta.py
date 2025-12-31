1670d7df41918798b4f6f5795a4dea181e9f801f
Namachan <nnkmr1126@gmail.com>
2025-12-30 23:45:49 -0500

    saved

--- scripts/read_fasta.py
+++ scripts/read_fasta.py
@@ -5,9 +5,9 @@ from pathlib import Path
 data_dir = Path("data") #this points to the folder that contains the three FASTA files
 
 for fasta_file in data_dir.glob("*.fasta"):
-   for record in SeqIO.parse(fasta_file, "fasta"):
+   for record in SeqIO.parse(fasta_file, "fasta"): 
 
-        print ("=" * 50)
+        print ("=" * 50) #Helps for readability
 
         print("File Name:", fasta_file.name)
         print("Sequence ID:", record.id)
@@ -16,3 +16,7 @@ for fasta_file in data_dir.glob("*.fasta"):
 
         print("First 10 amino acids:", record.seq[:10])
 
+
+#BRCA1 has a protein length of 1863
+#BRCA2 has a protein length of 3418
+#TP53 has a protein length of 393
